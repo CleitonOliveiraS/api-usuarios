b492b6fd3843b7dd1d44e3eb39f7dfa57ce307da
Cleiton <cleitonman@hotmail.com.br>
2026-06-05 05:53:26 -0300

    chore(config): adiciona configuração para deixar endpoints publicos.
    #1

---
+++ src/main/java/br/com/cleiton/api/usuarios/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package br.com.cleiton.api.usuarios.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // 1. Desabilita o CSRF (comum em APIs REST baseadas em Tokens/Stateless)
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // 2. Configura as regras de permissão dos endpoints
+                .authorizeHttpRequests(authorize -> authorize
+                        // Libera especificamente o POST na rota /usuarios
+                        .requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
+                        .requestMatchers("/error").permitAll() // 👈 ADICIONE ISSO: Libera as respostas de erro da validação
+
+                        // Exemplo: Libera qualquer requisição que comece com /publico/
+                        // .requestMatchers("/publico/**").permitAll()
+
+                        // Qualquer outra requisição do sistema exigirá autenticação
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
