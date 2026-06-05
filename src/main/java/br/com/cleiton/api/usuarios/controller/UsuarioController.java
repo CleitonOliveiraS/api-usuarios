b61e63ccf33a8dac8bef16b453aa0959bf6c067f
Cleiton <cleitonman@hotmail.com.br>
2026-06-05 06:33:30 -0300

    feat(usuario): adicionar UsuarioController, UsuarioService e UsuarioRepository
    - criar endpoint para cadastrar usuario
    - criar logica para gravar usuario e evitar duplicidade de email
    
    #1

---
+++ src/main/java/br/com/cleiton/api/usuarios/controller/UsuarioController.java
@@ -0,0 +1,38 @@
+package br.com.cleiton.api.usuarios.controller;
+
+import br.com.cleiton.api.usuarios.dto.UsuarioCadastroRequest;
+import br.com.cleiton.api.usuarios.dto.UsuarioResponse;
+import br.com.cleiton.api.usuarios.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponse> cadastar(@RequestBody @Valid UsuarioCadastroRequest usuarioCadastroRequest) {
+        UsuarioResponse usuario = usuarioService.cadastrar(usuarioCadastroRequest);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(usuario.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(usuario);
+    }
+
+}
